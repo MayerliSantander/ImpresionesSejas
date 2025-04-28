a11748a387c63d13e1be172eaa5ed829e33e929f
MayerliSantander <mayerlisantander0@gmail.com>
2025-04-28 10:49:43 -0400

    feat: add services

---
+++ PrinterQuoter/Services/MaterialService.cs
@@ -0,0 +1,67 @@
+﻿using Core.Dtos;
+using Core.Entities;
+using Core.Interfaces.Services;
+using Core.Interfaces.UseCases;
+
+namespace Services;
+
+public class MaterialService : IMaterialService
+{
+    private readonly IMaterialUseCase _materialUseCase;
+
+    public MaterialService(IMaterialUseCase materialUseCase)
+    {
+        _materialUseCase = materialUseCase;
+    }
+
+    public async ValueTask<Material> GetMaterialById(Guid id)
+    {
+        return await _materialUseCase.MaterialRepository.GetById(id);
+    }
+
+    public async Task<IEnumerable<Material>> GetAllMaterials()
+    {
+        return await _materialUseCase.MaterialRepository.GetAll();
+    }
+
+    public async Task<Material> CreateMaterial(MaterialDto materialDto)
+    {
+        Material material = materialDto.CreateMaterial();
+        await _materialUseCase.MaterialRepository.Add(material);
+        await _materialUseCase.Commitment();
+        return material;
+    }
+
+    public async Task<Material> UpdateMaterial(Guid idMaterial, MaterialDto materialDto)
+    {
+        Material material = materialDto.CreateMaterial();
+        material.Id = idMaterial;
+        Material materialToUpdate = await _materialUseCase.MaterialRepository.GetById(material.Id);
+        if (material.MaterialName != null && material.MaterialName != "")
+        {
+            materialToUpdate.MaterialName = material.MaterialName;
+        }
+        if (material.Size != null && material.Size != "")
+        {
+            materialToUpdate.Size = material.Size;
+        }
+        if (material.Type != null && material.Type != "")
+        {
+            materialToUpdate.Type = material.Type;
+        }
+        if (material.MaterialPrice != null && material.MaterialPrice > 0)
+        {
+            materialToUpdate.MaterialPrice = material.MaterialPrice;
+        }
+        await _materialUseCase.MaterialRepository.Update(materialToUpdate);
+        await _materialUseCase.Commitment();
+        return material;
+    }
+
+    public async Task DeleteMaterial(Guid materialId)
+    {
+        Material materialToDelete = await _materialUseCase.MaterialRepository.GetById(materialId);
+        _materialUseCase.MaterialRepository.Remove(materialToDelete);
+        await _materialUseCase.Commitment();
+    }
+}
---
+++ PrinterQuoter/Services/UserService.cs
@@ -0,0 +1,92 @@
+using Core.Dtos;
+using Core.Entities;
+using Core.Interfaces.Services;
+using Core.Interfaces.UseCases;
+
+namespace Services;
+
+public class UserService: IUserService
+{
+    private readonly IUserUseCase _userUseCase;
+
+    public UserService(IUserUseCase userUseCase)
+    {
+        _userUseCase = userUseCase;
+    }
+
+    public async ValueTask<User> GetUserById(Guid id)
+    {
+        return await _userUseCase.UserRepository.GetById(id);
+    }
+
+    public async Task<IEnumerable<User>> GetAllUsers()
+    {
+        return await _userUseCase.UserRepository.GetAll();
+    }
+
+    public async Task<User> CreateUser(UserDto userDto)
+    {
+        User user = userDto.CreateUser();
+        await _userUseCase.UserRepository.Add(user);
+        await _userUseCase.Commitment();
+        return user;
+    }
+
+    public async Task<User> UpdateUser(Guid idUser, UserDto userDto)
+    {
+        User user = userDto.CreateUser();
+        user.Id = idUser;
+        User userToUpdate = await _userUseCase.UserRepository.GetById(user.Id);
+        if (user.Email != null && user.Email != "")
+        {
+            userToUpdate.Email = user.Email;
+        }
+        if (user.UserName != null && user.UserName != "")
+        {
+            userToUpdate.UserName = user.UserName;
+        }
+        await _userUseCase.UserRepository.Update(userToUpdate);
+        await _userUseCase.Commitment();
+        return user;
+    }
+
+    public async Task DeleteUser(Guid userId)
+    {
+        User userToDelete = await _userUseCase.UserRepository.GetById(userId);
+        _userUseCase.UserRepository.Remove(userToDelete);
+        await _userUseCase.Commitment();
+    }
+
+    public async ValueTask<User> GetByGoogleId(string googleId)
+    {
+        IEnumerable<User> users = await _userUseCase.UserRepository.GetByGoogleId(googleId);
+        return users.FirstOrDefault();
+    }
+
+    public async Task<User> LoginOrRegisterGoogleAsync(string googleId, string name, string email)
+    {
+        IEnumerable<User> users = await _userUseCase.UserRepository.GetByGoogleId(googleId);
+        User user = users.FirstOrDefault();
+        if (user == null)
+        {
+            var clientRole = await _userUseCase.RoleRepository.GetRoleByDescription("client");
+            if (clientRole == null)
+                throw new InvalidOperationException("Rol client no existe.");
+
+            User newUser = new User
+            {
+                Id       = Guid.NewGuid(),
+                GoogleId = googleId,
+                UserName = name,
+                Email    = email,
+                Phone    = string.Empty,
+                Roles    = new List<Role> { clientRole }
+            };
+
+            await _userUseCase.UserRepository.Add(newUser);
+            await _userUseCase.Commitment();
+            return newUser;
+        }
+        return user;
+    }
+}
